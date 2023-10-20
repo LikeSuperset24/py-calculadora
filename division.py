c1ff5af38d44e2a88cd6163d3248603a1f0166c2
Juan Seja <juanpa_blo908@hotmail.com>
2023-10-20 12:31:56 -0600

    Corrección Main - Final

--- division.py
+++ division.py
@@ -1,7 +1,7 @@
 # Función de división
 # Toma dos argumentos "dividendo" y "divisor".
 def division(dividendo, divisor):
-    if y != 0:
+    if divisor != 0:
         return dividendo / divisor
     else:
         return "¡Error! No puedes dividir por cero."
