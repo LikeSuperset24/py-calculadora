f1b7555a33bbd4c08ea0a0fb83e4ab798e3c2e48
Miguel <20460307@colima.tecnm.mx>
2023-10-20 12:00:03 -0600

    Funcion resta

---
+++ resta.py
@@ -0,0 +1,5 @@
+#Función de resta
+#Recibe los valores del minuendo y sustraendo de la resta y devuelve el valor de la diferencia
+def resta(minuendo,sustraendo):
+    diferencia=minuendo-sustraendo
+    return diferencia
