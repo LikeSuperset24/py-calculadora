589601c96726ab3e027941fa0a809598aea8c2d9
Juan Seja <juanpa_blo908@hotmail.com>
2023-10-20 12:18:50 -0600

    Documentación de los métodos

--- division.py
+++ division.py
@@ -1,3 +1,5 @@
+# Función de división
+# Toma dos argumentos "dividendo" y "divisor".
 def division(x, y):
     if y != 0:
         return x / y
--- resta.py
+++ resta.py
@@ -1,5 +1,5 @@
-#Función de resta para calculadora
-#Recibe los valores del minuendo y sustraendo de la resta y devuelve el valor de la diferencia
-def resta(minuendo,sustraendo):
-    diferencia=minuendo-sustraendo
+# Función de resta
+# Toma dos argumentos "minuendo" y "sustrayendo".
+def resta(minuendo, sustraendo):
+    diferencia = minuendo - sustraendo
     return diferencia
